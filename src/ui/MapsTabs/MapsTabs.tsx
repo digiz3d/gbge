009ec12436f7cccaf7056bf176e016eb227292e5
Léandre Daumont <1338620+digiz3d@users.noreply.github.com>
2025-07-12 22:41:23 +0200

    feat: display selected tile coordinates

--- src/ui/MapsTabs/MapsTabs.tsx
+++ src/ui/MapsTabs/MapsTabs.tsx
@@ -1,14 +1,21 @@
 import { useAtomValue, useSetAtom } from "jotai";
 import {
   currentEditedMapIndexAtom,
+  currentSelectionAtom,
   unfocusMapToEditAtom,
 } from "../../state/ui";
 import { mapsAtom } from "../../state/map";
+
+const HARDWARE_OFFSETS = {
+  x: 8,
+  y: 16,
+} as const;
 
 export function MapsTabs() {
   const maps = useAtomValue(mapsAtom);
   const currentMapIndex = useAtomValue(currentEditedMapIndexAtom);
   const unfocusMapToEdit = useSetAtom(unfocusMapToEditAtom);
+  const currentSelection = useAtomValue(currentSelectionAtom);
 
   if (currentMapIndex === null) return null;
 
@@ -23,6 +30,18 @@ export function MapsTabs() {
         Back
       </button>
       <div>Editing map "{currentMap.id}"</div>
+      {currentSelection.mode === "mapTiles" &&
+        currentSelection.indexes.length > 0 && (
+          <div>
+            Coordinates: X:
+            {(currentSelection.indexes[0] % currentMap.size.width) * 8 +
+              HARDWARE_OFFSETS.x}{" "}
+            Y:
+            {Math.floor(
+              (currentSelection.indexes[0] / currentMap.size.width) * 8
+            ) + HARDWARE_OFFSETS.y}
+          </div>
+        )}
     </div>
   );
 }
